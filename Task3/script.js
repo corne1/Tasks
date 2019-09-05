c2ec80a76cdd65e69849c51e360c96a544d02e0b
nnm74 <33349497+nnm74@users.noreply.github.com>
2019-09-05 20:35:46 +0300

    3 Task is complite

---
+++ Task3/script.js
@@ -0,0 +1,81 @@
+'use strict';
+let money,
+    time;
+
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", "");
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+  while (isNaN(money) || money == "" || money == null) {
+    money = +prompt("Ваш бюджет на месяц?", "");
+  }
+}
+start();
+
+let appData = {
+  budget : money,
+  timeData: time,
+  expenses:{},
+  optionalExpenses:{},
+  income: [],
+  savings: true
+};
+
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+      b = prompt("Во сколько обойдется?");
+
+    if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b) != null) &&
+      a != '' && b != '' && a.length < 50) {
+      console.log("done");
+      appData.expenses[a] = b;
+    } else {
+      i = i - 1;
+    }
+  }
+}
+
+chooseExpenses();
+
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+  alert("Ежедневный бюджет: " + appData.moneyPerDay);
+}
+detectDayBudget();
+
+function detectLevel() {
+  if (appData.moneyPerDay < 100) {
+    console.log("минимальный уровень достатка");
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+  } else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+detectLevel();
+
+function checkSavings() {
+  if(appData.savings == true){
+    let save = +prompt("Какова сумма накоплений?"),
+        percent = +prompt("Под какой процент?");
+    appData.monthInCome = save/100/12*percent;
+    alert("Доход в месяц с вашего депозита: " + appData.monthInCome);
+  }
+}
+checkSavings();
+
+function chooseOptExpenses() {
+  for (let i = 0; i < 3; i++) {
+    let answer = prompt("Статья необязательных расходов?");
+    if ((typeof (answer)) === 'string' && (typeof (answer)) != null &&
+      answer != '' && b != '' && answer.length < 50) {
+      console.log("done");
+      appData.expenses[i+1] = answer;
+    } else {
+      i = i - 1;
+    }
+  }
+}
